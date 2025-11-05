29b94e5b135b3b522821294e95ce5288ea9e9bfd
khghm <khghm@github.com>
2025-11-05 15:13:11 +0330

    feat: initial commit with Vite + Vue

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="سلام دنیا! 🌍" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 50px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+img {
+  width: 100px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <p>این اولین پروژه تو با Vite و Vue است!</p>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String
+  }
+}
+</script>
